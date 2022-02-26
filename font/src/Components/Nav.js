5a00bfdd9c65d367a6e01135e579d33366b2d2cb
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-02-26 14:23:19 +0530

    Update Nav.js

--- font/src/Components/Nav.js
+++ font/src/Components/Nav.js
@@ -8,20 +8,20 @@ export default function Nav() {
     <nav className="navbar bg-dark">
       <h1>
         <Link to="/">
-          <i className="fas fa-code"> </i> DevConnector{" "}
-        </Link>{" "}
-      </h1>{" "}
+          <i className="fas fa-code"> </i> DevConnector
+        </Link>
+      </h1>
       <ul>
         <li>
-          <Link to="!#"> Developers </Link>{" "}
-        </li>{" "}
+          <Link to="!#"> Developers </Link>
+        </li>
         <li>
-          <Link to="/register"> Register </Link>{" "}
-        </li>{" "}
+          <Link to="/register"> Register </Link>
+        </li>
         <li>
-          <Link to="/login"> Login </Link>{" "}
-        </li>{" "}
-      </ul>{" "}
+          <Link to="/login"> Login </Link>
+        </li>
+      </ul>
     </nav>
   );
 }
