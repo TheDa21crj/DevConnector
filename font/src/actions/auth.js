d8cfced0f0b7014b3d56e78088952da2cb48dfd7
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-02-26 14:23:28 +0530

    working on logout

--- font/src/actions/auth.js
+++ font/src/actions/auth.js
@@ -7,6 +7,7 @@ import {
     AUTH_ERROR,
     LOGIN_SUCCESS,
     LOGIN_FAIL,
+    LOGOUT,
 } from "./types";
 import { setAlert } from "./alert";
 
@@ -81,4 +82,9 @@ export const login = (email, password) => async(dispatch) => {
             type: LOGIN_FAIL,
         });
     }
+};
+
+// LOGOUT
+export const logout = () => (dispatch) => {
+    dispatch({ type: LOGOUT });
 };
