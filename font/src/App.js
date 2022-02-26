81fb942f13879aecedba3f8710fdb858f9444ede
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-02-26 16:28:34 +0530

    route /dashboard created

--- font/src/App.js
+++ font/src/App.js
@@ -38,6 +38,9 @@ function App() {
               <Route exact path="/login">
                 <Login />
               </Route>
+              <Route exact path="/dashboard">
+                <Dashboard />
+              </Route>
             </Switch>
           </section>
         </Fragment>
