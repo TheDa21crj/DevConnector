a326167b874e7feb362337af3d807107f990aa7b
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-02-26 14:23:08 +0530

    Update Nav.js

--- font/src/Components/Nav.js
+++ font/src/Components/Nav.js
@@ -1,25 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 
 export default function Nav() {
   return (
     <nav className="navbar bg-dark">
       <h1>
         <Link to="/">
-          <i className="fas fa-code"></i> DevConnector
-        </Link>
-      </h1>
+          <i className="fas fa-code"> </i> DevConnector{" "}
+        </Link>{" "}
+      </h1>{" "}
       <ul>
         <li>
-          <Link to="!#">Developers</Link>
-        </li>
+          <Link to="!#"> Developers </Link>{" "}
+        </li>{" "}
         <li>
-          <Link to="/register">Register</Link>
-        </li>
+          <Link to="/register"> Register </Link>{" "}
+        </li>{" "}
         <li>
-          <Link to="/login">Login</Link>
-        </li>
-      </ul>
+          <Link to="/login"> Login </Link>{" "}
+        </li>{" "}
+      </ul>{" "}
     </nav>
   );
 }
