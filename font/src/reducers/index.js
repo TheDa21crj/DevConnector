581c1b51a4aa13c59ec22ac06ede9e643daac1a6
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-03-01 06:20:34 +0100

    Update index.js

--- font/src/reducers/index.js
+++ font/src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import alert from "./alert";
 import auth from "./auth";
+import profile from "./profile";
 import { logout } from "./../actions/auth";
 
 export default combineReducers({
     alert,
     auth,
+    profile,
 });
