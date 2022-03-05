ab1b27d765bfa1adaf97595e32305140857c07ba
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-03-05 15:04:46 +0100

    location stat. changed

--- font/src/Components/Spinner.js
+++ font/src/Components/Spinner.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from "react";
-import spinner from "./spinner.gif";
+import spinner from "./../img/spinner.gif";
 
 export default function Spinner() {
   return <div>Spinner</div>;
