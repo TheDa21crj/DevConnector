c101cda7a8d43f10237c33bd1461b9718c6990a4
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-03-05 17:12:09 +0100

    Update CreateProfile.js

--- font/src/Components/Profile/CreateProfile.js
+++ font/src/Components/Profile/CreateProfile.js
@@ -204,7 +204,6 @@ const CreateProfile = (props) => {
         )}
 
         <input type="submit" className="btn btn-primary my-1" />
-
         <a className="btn btn-light my-1" href="dashboard.html">
           Go Back
         </a>
