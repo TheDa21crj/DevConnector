3323fba57647e9e172ea02806cb07efdfa6e3c8a
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-03-05 15:10:55 +0100

    Update Dashboard.js

--- font/src/Components/Dashboard/Dashboard.js
+++ font/src/Components/Dashboard/Dashboard.js
@@ -1,14 +1,25 @@
-import React, { useEffect } from "react";
+import React, { Fragment, useEffect } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { getCurrentProfile } from "./../../actions/profile";
+import Spinner from "./../Spinner";
 
-const Dashboard = ({ getCurrentProfile, auth, profile }) => {
+const Dashboard = ({
+  getCurrentProfile,
+  auth,
+  profile: { profile, loading },
+}) => {
   useEffect(() => {
     getCurrentProfile();
   }, []);
 
-  return <div> Dashboard </div>;
+  return loading && profile === null ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <h1 className="large text-primary">Dashboard</h1>
+    </Fragment>
+  );
 };
 
 Dashboard.propTypes = {
