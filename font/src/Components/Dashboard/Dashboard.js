795df67150187f17ee1b64f9b94cb4294317e3ae
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-03-05 15:29:20 +0100

    CLEAR_PROFILE route added

--- font/src/Components/Dashboard/Dashboard.js
+++ font/src/Components/Dashboard/Dashboard.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useEffect } from "react";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { getCurrentProfile } from "./../../actions/profile";
@@ -6,7 +7,7 @@ import Spinner from "./../Spinner";
 
 const Dashboard = ({
   getCurrentProfile,
-  auth,
+  auth: { user },
   profile: { profile, loading },
 }) => {
   useEffect(() => {
@@ -18,6 +19,17 @@ const Dashboard = ({
   ) : (
     <Fragment>
       <h1 className="large text-primary">Dashboard</h1>
+      <p className="lead">Welcome {user && user.name}</p>
+      {profile !== null ? (
+        <Fragment>has</Fragment>
+      ) : (
+        <Fragment>
+          <p>You have not yet setup a profile, please add some info</p>
+          <Link to="./create-profile" className="btn btn-primary my-1">
+            Create Profile
+          </Link>
+        </Fragment>
+      )}
     </Fragment>
   );
 };
