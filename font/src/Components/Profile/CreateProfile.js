be5d95274b9b412773640d14a1d9b049f178834c
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-03-06 09:49:32 +0100

    Update CreateProfile.js

--- font/src/Components/Profile/CreateProfile.js
+++ font/src/Components/Profile/CreateProfile.js
@@ -46,29 +46,29 @@ const CreateProfile = ({ createProfile, history }) => {
   };
   return (
     <Fragment>
-      <h1 className="large text-primary"> Create Your Profile </h1>{" "}
+      <h1 className="large text-primary"> Create Your Profile </h1>
       <p className="lead">
         <i className="fas fa-user"> </i> Let's get some information to make your
-        profile stand out{" "}
-      </p>{" "}
-      <small> * = required field </small>{" "}
+        profile stand out
+      </p>
+      <small> * = required field </small>
       <form className="form" onSubmit={Submit}>
         <div className="form-group">
           <select name="status" value={status} onChange={onChange}>
-            <option value="0"> * Select Professional Status </option>{" "}
-            <option value="Developer"> Developer </option>{" "}
-            <option value="Junior Developer"> Junior Developer </option>{" "}
-            <option value="Senior Developer"> Senior Developer </option>{" "}
-            <option value="Manager"> Manager </option>{" "}
-            <option value="Student or Learning"> Student or Learning </option>{" "}
-            <option value="Instructor"> Instructor or Teacher </option>{" "}
-            <option value="Intern"> Intern </option>{" "}
-            <option value="Other"> Other </option>{" "}
-          </select>{" "}
+            <option value="0"> * Select Professional Status </option>
+            <option value="Developer"> Developer </option>
+            <option value="Junior Developer"> Junior Developer </option>
+            <option value="Senior Developer"> Senior Developer </option>
+            <option value="Manager"> Manager </option>
+            <option value="Student or Learning"> Student or Learning </option>
+            <option value="Instructor"> Instructor or Teacher </option>
+            <option value="Intern"> Intern </option>
+            <option value="Other"> Other </option>
+          </select>
           <small className="form-text">
-            Give us an idea of where you are at in your career{" "}
-          </small>{" "}
-        </div>{" "}
+            Give us an idea of where you are at in your career
+          </small>
+        </div>
         <div className="form-group">
           <input
             type="text"
@@ -76,11 +76,11 @@ const CreateProfile = ({ createProfile, history }) => {
             name="company"
             value={company}
             onChange={onChange}
-          />{" "}
+          />
           <small className="form-text">
-            Could be your own company or one you work for{" "}
-          </small>{" "}
-        </div>{" "}
+            Could be your own company or one you work for
+          </small>
+        </div>
         <div className="form-group">
           <input
             type="text"
@@ -88,11 +88,11 @@ const CreateProfile = ({ createProfile, history }) => {
             name="website"
             value={website}
             onChange={onChange}
-          />{" "}
+          />
           <small className="form-text">
-            Could be your own or a company website{" "}
-          </small>{" "}
-        </div>{" "}
+            Could be your own or a company website
+          </small>
+        </div>
         <div className="form-group">
           <input
             type="text"
@@ -100,11 +100,11 @@ const CreateProfile = ({ createProfile, history }) => {
             name="location"
             value={location}
             onChange={onChange}
-          />{" "}
+          />
           <small className="form-text">
-            City & state suggested(eg.Boston, MA){" "}
-          </small>{" "}
-        </div>{" "}
+            City & state suggested(eg.Boston, MA)
+          </small>
+        </div>
         <div className="form-group">
           <input
             type="text"
@@ -112,11 +112,11 @@ const CreateProfile = ({ createProfile, history }) => {
             name="skills"
             value={skills}
             onChange={onChange}
-          />{" "}
+          />
           <small className="form-text">
-            Please use comma separated values(eg.HTML, CSS, JavaScript, PHP){" "}
-          </small>{" "}
-        </div>{" "}
+            Please use comma separated values(eg.HTML, CSS, JavaScript, PHP)
+          </small>
+        </div>
         <div className="form-group">
           <input
             type="text"
@@ -124,20 +124,20 @@ const CreateProfile = ({ createProfile, history }) => {
             name="githubusername"
             value={githubusername}
             onChange={onChange}
-          />{" "}
+          />
           <small className="form-text">
             If you want your latest repos and a Github link, include your
-            username{" "}
-          </small>{" "}
-        </div>{" "}
+            username
+          </small>
+        </div>
         <div className="form-group">
           <textarea
             placeholder="A short bio of yourself"
             name="bio"
             value={bio}
             onChange={onChange}
-          ></textarea>{" "}
-          <small className="form-text"> Tell us a little about yourself </small>{" "}
+          ></textarea>
+          <small className="form-text"> Tell us a little about yourself </small>
         </div>
         <div className="my-2">
           <button
@@ -145,69 +145,69 @@ const CreateProfile = ({ createProfile, history }) => {
             type="button"
             className="btn btn-light"
           >
-            Add Social Network Links{" "}
-          </button>{" "}
-          <span> Optional </span>{" "}
+            Add Social Network Links
+          </button>
+          <span> Optional </span>
         </div>
         {displaySocialInp && (
           <Fragment>
             <div className="form-group social-input">
-              <i className="fab fa-twitter fa-2x"> </i>{" "}
+              <i className="fab fa-twitter fa-2x"> </i>
               <input
                 type="text"
                 placeholder="Twitter URL"
                 name="twitter"
                 value={twitter}
                 onChange={onChange}
-              />{" "}
+              />
             </div>
             <div className="form-group social-input">
-              <i className="fab fa-facebook fa-2x"> </i>{" "}
+              <i className="fab fa-facebook fa-2x"> </i>
               <input
                 type="text"
                 placeholder="Facebook URL"
                 name="facebook"
                 value={facebook}
                 onChange={onChange}
-              />{" "}
+              />
             </div>
             <div className="form-group social-input">
-              <i className="fab fa-youtube fa-2x"> </i>{" "}
+              <i className="fab fa-youtube fa-2x"> </i>
               <input
                 type="text"
                 placeholder="YouTube URL"
                 name="youtube"
                 value={youtube}
                 onChange={onChange}
-              />{" "}
+              />
             </div>
             <div className="form-group social-input">
-              <i className="fab fa-linkedin fa-2x"> </i>{" "}
+              <i className="fab fa-linkedin fa-2x"> </i>
               <input
                 type="text"
                 placeholder="Linkedin URL"
                 name="linkedin"
                 value={linkedin}
                 onChange={onChange}
-              />{" "}
+              />
             </div>
             <div className="form-group social-input">
-              <i className="fab fa-instagram fa-2x"> </i>{" "}
+              <i className="fab fa-instagram fa-2x"> </i>
               <input
                 type="text"
                 placeholder="Instagram URL"
                 name="instagram"
                 value={instagram}
                 onChange={onChange}
-              />{" "}
-            </div>{" "}
+              />
+            </div>
           </Fragment>
         )}
         <input type="submit" className="btn btn-primary my-1" />
         <a className="btn btn-light my-1" href="dashboard.html">
-          Go Back{" "}
-        </a>{" "}
-      </form>{" "}
+          Go Back
+        </a>
+      </form>
     </Fragment>
   );
 };
