d5c89974bdaa9c6402fc2eb469f3e50bedc65fdf
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-03-07 21:53:02 +0100

    useeffect added

--- font/src/Components/Profile/EditProfile.js
+++ font/src/Components/Profile/EditProfile.js
@@ -29,7 +29,23 @@ const EditProfile = ({
 
   useEffect(() => {
     getCurrentProfile();
-  });
+
+    setformData({
+      company: loading || !profile.company ? "" : profile.company,
+      website: loading || !profile.website ? "" : profile.website,
+      location: loading || !profile.location ? "" : profile.location,
+      status: loading || !profile.status ? "" : profile.status,
+      skills: loading || !profile.skills ? "" : profile.skills.join(","),
+      githubusername:
+        loading || !profile.githubusername ? "" : profile.githubusername,
+      bio: loading || !profile.bio ? "" : profile.bio,
+      twitter: loading || !profile.social ? "" : profile.social.twitter,
+      facebook: loading || !profile.social ? "" : profile.social.facebook,
+      linkedin: loading || !profile.social ? "" : profile.social.linkedin,
+      youtube: loading || !profile.social ? "" : profile.social.youtube,
+      instagram: loading || !profile.social ? "" : profile.social.instagram,
+    });
+  }, [loading]);
 
   const {
     company,
