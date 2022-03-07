92c0e435d99a178cc4372bf21c9c048e8f0dc8f6
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-03-07 21:54:04 +0100

    Update App.js

--- font/src/App.js
+++ font/src/App.js
@@ -31,26 +31,26 @@ function App() {
           <Nav />
           <Route exact path="/">
             <Home />
-          </Route>{" "}
+          </Route>
           <section className="container">
             <Alert />
             <Switch>
               <Route exact path="/register">
                 <Register />
-              </Route>{" "}
+              </Route>
               <Route exact path="/login">
                 <Login />
-              </Route>{" "}
-              <PrivateRoute exact path="/dashboard" component={Dashboard} />{" "}
+              </Route>
+              <PrivateRoute exact path="/dashboard" component={Dashboard} />
               <PrivateRoute
                 exact
                 path="/create-profile"
                 component={CreateProfile}
-              />{" "}
-            </Switch>{" "}
-          </section>{" "}
-        </Fragment>{" "}
-      </Router>{" "}
+              />
+            </Switch>
+          </section>
+        </Fragment>
+      </Router>
     </Provider>
   );
 }
