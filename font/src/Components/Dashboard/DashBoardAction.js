65e55c4bff412d71df5e266f12e2687a82f04383
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-03-07 21:32:41 +0100

    Update DashBoardAction.js

---
+++ font/src/Components/Dashboard/DashBoardAction.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const DashBoardAction = (props) => {
+  return <div>DashBoardAction</div>;
+};
+
+export default DashBoardAction;
